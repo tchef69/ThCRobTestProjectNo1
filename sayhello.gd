903463cc9d88ca61c5b42e9a1af65ee2ba511221
Thomas Chef <thomas.chef@gmail.com>
2019-11-10 09:24:00 +0100

    Done the Scripting tutorial.

---
+++ sayhello.gd
@@ -0,0 +1,12 @@
+
+extends Panel
+
+func _ready():
+    get_node("Button").connect("pressed", self, "_on_Button_pressed")
+
+func _on_Button_pressed():
+    get_node("Label_button").text = "HELLO!"
+
+# This has been created automatically my the GUI
+func _on_Button_AutoConnect_pressed():
+	get_node("Label_button_Auto_Connect").text = "HELLO!"
