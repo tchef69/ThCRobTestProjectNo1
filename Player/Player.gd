e2e1166f2688d0727520f13a3c3fa875d0d47101
Thomas Chef <thomas.chef@gmail.com>
2019-11-16 09:45:54 +0100

    Basic bare game works.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
 func _ready():
     screen_size = get_viewport_rect().size
+    hide()
 
 
 func _process(delta):
@@ -26,4 +29,22 @@ func _process(delta):
     position += velocity * delta
     position.x = clamp(position.x, 0, screen_size.x)
     position.y = clamp(position.y, 0, screen_size.y)
+    
+    if velocity.x != 0:
+        $AnimatedSprite.animation = "right"
+        $AnimatedSprite.flip_v = false
+        $AnimatedSprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+        $AnimatedSprite.animation = "up"
+        $AnimatedSprite.flip_v = velocity.y > 0
 
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
+
+func _on_Player_body_entered(body):
+    hide()  # Player disappears after being hit.
+    emit_signal("hit")
+    $CollisionShape2D.set_deferred("disabled", true)
+    
